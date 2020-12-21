a5d90d98a30292ccfaa149329e30a9c171e181c1
Alba De biase <debiasealba@gmail.com>
2020-12-21 18:01:22 +0100

    creazione file blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>home</title>
+        <!-- funzione che specifica che il vpercorso parte dalla cartella public -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        
+    </body>
+</html>
